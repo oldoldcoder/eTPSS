904c1bb5421857f4b0df5fa109a6e69d00d23c12
oldoldcoder <2742598536@qq.com>
2024-01-20 11:40:10 +0800

    增加对负数的支持

--- eTPSS.c
+++ eTPSS.c
@@ -56,14 +56,87 @@ static int generate_array(){
 }
 static void et_refresh_x(eTPSS * a){
 
-    BN_mod_add(a->CS1.x,a->CS1.x,a->CS1.r1,MOD,CTX);
-    BN_mod_sub(a->CS1.x,a->CS1.x,a->CS1.r2,MOD,CTX);
+    // 判断负值然后刷新
+    int neg_flag = 0;
+    char * x1;
 
-    BN_mod_add(a->CS2.x,a->CS2.x,a->CS2.r1,MOD,CTX);
-    BN_mod_sub(a->CS2.x,a->CS2.x,a->CS2.r2,MOD,CTX);
+    x1 = BN_bn2dec(a->CS1.x);
+    fprintf(stdout,"0.init :a->CS1.x:%s\n",x1);
+    fflush(stdout);  // 刷新标准输出流
+    x1 = BN_bn2dec(a->CS2.x);
+    fprintf(stdout,"0.init :a->CS2.x:%s\n",x1);
+    fflush(stdout);  // 刷新标准输出流
+    x1 = BN_bn2dec(a->CS3.x);
+    fprintf(stdout,"0.init :a->CS3.x:%s\n",x1);
+    fflush(stdout);  // 刷新标准输出流
 
-    BN_mod_add(a->CS3.x,a->CS3.x,a->CS3.r1,MOD,CTX);
-    BN_mod_sub(a->CS3.x,a->CS3.x,a->CS3.r2,MOD,CTX);
+
+
+
+    BN_add(a->CS1.x,a->CS1.x,a->CS1.r1);
+    if(BN_is_negative(a->CS1.x))
+        neg_flag = 1;
+    BN_set_negative(a->CS1.x,0);
+    BN_nnmod(a->CS1.x,a->CS1.x,MOD,CTX);
+    BN_set_negative(a->CS1.x,neg_flag);
+    x1 = BN_bn2dec(a->CS1.x);
+    fprintf(stdout,"1.add :a->CS1.x:%s\n",x1);
+    fflush(stdout);  // 刷新标准输出流
+
+    BN_sub(a->CS1.x,a->CS1.x,a->CS1.r2);
+    neg_flag = 0;
+    if(BN_is_negative(a->CS1.x))
+        neg_flag = 1;
+    BN_set_negative(a->CS1.x,0);
+    BN_nnmod(a->CS1.x,a->CS1.x,MOD,CTX);
+    BN_set_negative(a->CS1.x,neg_flag);
+    x1 = BN_bn2dec(a->CS1.x);
+    fprintf(stdout,"2.sub :a->CS1.x:%s\n",x1);
+    fflush(stdout);  // 刷新标准输出流
+    neg_flag = 0;
+
+    BN_add(a->CS2.x,a->CS2.x,a->CS2.r1);
+    if(BN_is_negative(a->CS2.x))
+        neg_flag = 1;
+    BN_set_negative(a->CS2.x,0);
+    BN_nnmod(a->CS2.x,a->CS2.x,MOD,CTX);
+    BN_set_negative(a->CS2.x,neg_flag);
+    x1 = BN_bn2dec(a->CS2.x);
+    fprintf(stdout,"1.add :a->CS2.x:%s\n",x1);
+    fflush(stdout);  // 刷新标准输出流
+    BN_sub(a->CS2.x,a->CS2.x,a->CS2.r2);
+    neg_flag = 0;
+    if(BN_is_negative(a->CS2.x))
+        neg_flag = 1;
+    BN_set_negative(a->CS2.x,0);
+    BN_nnmod(a->CS2.x,a->CS2.x,MOD,CTX);
+    BN_set_negative(a->CS2.x,neg_flag);
+    x1 = BN_bn2dec(a->CS2.x);
+    fprintf(stdout,"2.sub :a->CS2.x:%s\n",x1);
+    fflush(stdout);  // 刷新标准输出流
+    neg_flag = 0;
+
+    BN_add(a->CS3.x,a->CS3.x,a->CS3.r1);
+    if(BN_is_negative(a->CS3.x))
+        neg_flag = 1;
+    BN_set_negative(a->CS3.x,0);
+    BN_nnmod(a->CS3.x,a->CS3.x,MOD,CTX);
+    BN_set_negative(a->CS3.x,neg_flag);
+    x1 = BN_bn2dec(a->CS3.x);
+    fprintf(stdout,"1.add :a->CS3.x:%s\n",x1);
+    fflush(stdout);  // 刷新标准输出流
+    BN_sub(a->CS3.x,a->CS3.x,a->CS3.r2);
+    neg_flag = 0;
+    if(BN_is_negative(a->CS3.x))
+        neg_flag = 1;
+    BN_set_negative(a->CS3.x,0);
+    BN_nnmod(a->CS3.x,a->CS3.x,MOD,CTX);
+    BN_set_negative(a->CS3.x,neg_flag);
+    x1 = BN_bn2dec(a->CS3.x);
+    fprintf(stdout,"2.sub :a->CS3.x:%s\n",x1);
+    fflush(stdout);  // 刷新标准输出流
+    neg_flag = 0;
+    /**/
 }
 
 int init_eTPSS(eTPSS * var){
@@ -95,15 +168,23 @@ int free_eTPSS(eTPSS * var){
 }
 
 int et_Share(eTPSS * var,BIGNUM * num){
+
     int ret = 1;
+    // num是否是负数
+    int neg_flag = 0;
     BIGNUM * tmp = BN_new();
-    BN_copy(tmp,num);
+    if(BN_is_negative(num))
+        neg_flag = 1;
     BIGNUM * split1 = var->CS1.x;
     BIGNUM * split2 = var->CS2.x;
     BIGNUM * split3 = var->CS3.x;
+    BN_set_negative(num,0);
     if(!BN_nnmod(num,num,MOD,CTX)){
         goto end;
     }
+    BN_copy(tmp,num);
+    BN_set_negative(num,neg_flag == 1?1:0);
+
     // 第一次随机分割
     if(!BN_rand_range(split1,tmp)){
         fprintf(stderr,"process of split have some trouble");
@@ -119,14 +200,9 @@ int et_Share(eTPSS * var,BIGNUM * num){
 
     BN_sub(tmp,tmp,split2);
     BN_copy(split3,tmp);
-
-    // TODO 验证分割的正确性
-    BN_add(tmp,split1,split2);
-    BN_add(tmp,tmp,split3);
-    if(BN_cmp(tmp,num) != 0){
-        fprintf(stderr,"result of split is not equal original value");
-        goto end;
-    }
+    BN_set_negative(split1,neg_flag);
+    BN_set_negative(split2,neg_flag);
+    BN_set_negative(split3,neg_flag);
     ret = 0;
 end:
     BN_free(tmp);
@@ -135,18 +211,37 @@ end:
 
 int et_Recover(BIGNUM *num,eTPSS *var){
     int ret = 1;
+    int neg_flag = 0;
+
     BIGNUM * tmp = BN_new();
     BIGNUM * halfMod = BN_new();
     BN_copy(halfMod,MOD);
     BN_div_word(halfMod,2);
-    BN_mod_add(tmp,var->CS1.x,var->CS2.x,MOD,CTX);
-    BN_mod_add(tmp,tmp,var->CS3.x,MOD,CTX);
-    if(BN_cmp(tmp,halfMod) >= 0){
+    // 设置负值
+
+    BN_add(tmp,var->CS1.x,var->CS2.x);
+    if(BN_is_negative(tmp))
+        neg_flag = 1;
+    BN_set_negative(tmp,0);
+    BN_nnmod(tmp,tmp,MOD,CTX);
+    BN_set_negative(tmp,neg_flag);
+
+    neg_flag = 0;
+    BN_add(tmp,tmp,var->CS3.x);
+    if(BN_is_negative(tmp))
+        neg_flag = 1;
+    BN_set_negative(tmp,0);
+    BN_nnmod(tmp,tmp,MOD,CTX);
+    BN_set_negative(tmp,neg_flag);
+
+
+    if(neg_flag == 0 && BN_cmp(tmp,halfMod) >= 0){
         if(! BN_sub(num, tmp,halfMod))
             goto end;
     }else{
         BN_copy(num,tmp);
     }
+
     ret = 0;
 end:
     BN_free(tmp);
@@ -156,37 +251,81 @@ end:
 
 int et_Add(eTPSS *res,eTPSS * a,eTPSS *b){
 
-    if(!BN_mod_add(res->CS1.x,a->CS1.x,b->CS1.x,MOD,CTX) ||
-    !BN_mod_add(res->CS2.x,a->CS2.x,b->CS2.x,MOD,CTX) ||
-    !BN_mod_add(res->CS3.x,a->CS3.x,b->CS3.x,MOD,CTX)){
+    int neg_flag[3];
+    if(!BN_add(res->CS1.x,a->CS1.x,b->CS1.x) ||
+    !BN_add(res->CS2.x,a->CS2.x,b->CS2.x) ||
+    !BN_add(res->CS3.x,a->CS3.x,b->CS3.x)){
         return ETPSS_ERROR;
     }
+    if(BN_is_negative(res->CS1.x))
+        neg_flag[0] = 1;
+    if(BN_is_negative(res->CS2.x))
+        neg_flag[1] = 1;
+    if(BN_is_negative(res->CS3.x))
+        neg_flag[2] = 1;
+    BN_set_negative(res->CS1.x,0);
+    BN_set_negative(res->CS2.x,0);
+    BN_set_negative(res->CS3.x,0);
+    BN_nnmod(res->CS1.x,res->CS1.x,MOD,CTX);
+    BN_nnmod(res->CS2.x,res->CS2.x,MOD,CTX);
+    BN_nnmod(res->CS3.x,res->CS3.x,MOD,CTX);
+
+    BN_set_negative(res->CS1.x,neg_flag[0]);
+    BN_set_negative(res->CS2.x,neg_flag[1]);
+    BN_set_negative(res->CS3.x,neg_flag[2]);
     return ETPSS_SUCCESS;
 }
 
 int et_ScalP(eTPSS *res,eTPSS *var,BIGNUM *num){
-    if(!BN_mod_mul(res->CS1.x,var->CS1.x,num,MOD,CTX) ||
-    !BN_mod_mul(res->CS2.x,var->CS2.x,num,MOD,CTX) ||
-    !BN_mod_mul(res->CS3.x,var->CS3.x,num,MOD,CTX)){
+    int neg_flag[3];
+    if(!BN_mul(res->CS1.x,var->CS1.x,num,CTX) ||
+       !BN_mul(res->CS2.x,var->CS2.x,num,CTX) ||
+       !BN_mul(res->CS3.x,var->CS3.x,num,CTX)){
         return ETPSS_ERROR;
     }
+    if(BN_is_negative(res->CS1.x))
+        neg_flag[0] = 1;
+    if(BN_is_negative(res->CS2.x))
+        neg_flag[1] = 1;
+    if(BN_is_negative(res->CS3.x))
+        neg_flag[2] = 1;
+    BN_set_negative(res->CS1.x,0);
+    BN_set_negative(res->CS2.x,0);
+    BN_set_negative(res->CS3.x,0);
+    BN_nnmod(res->CS1.x,res->CS1.x,MOD,CTX);
+    BN_nnmod(res->CS2.x,res->CS2.x,MOD,CTX);
+    BN_nnmod(res->CS3.x,res->CS3.x,MOD,CTX);
+
+    BN_set_negative(res->CS1.x,neg_flag[0]);
+    BN_set_negative(res->CS2.x,neg_flag[1]);
+    BN_set_negative(res->CS3.x,neg_flag[2]);
     return ETPSS_SUCCESS;
 }
 
 int et_Mul(eTPSS *res,eTPSS *a,eTPSS *b){
+    int neg_flag = 0;
+    BIGNUM * tmp = BN_new();
+    et_Recover(tmp,a);
+
+    et_Recover(tmp,b);
+
     if(a->is_multi_res == 1){
         // 生成随机序列
         generate_array();
         a->CS1.r1 = rand_array[0];
+
         a->CS1.r2 = rand_array[2];
 
         a->CS2.r1 = rand_array[1];
+
         a->CS2.r2 = rand_array[0];
 
         a->CS3.r1 = rand_array[2];
+
         a->CS3.r2 = rand_array[1];
         // 通过r扰动刷新x的值
         et_refresh_x(a);
+        et_Recover(tmp,a);
         free_array();
     }
     if(b->is_multi_res == 1){
@@ -211,24 +350,53 @@ int et_Mul(eTPSS *res,eTPSS *a,eTPSS *b){
     BIGNUM  * t2 = BN_new();
     BIGNUM  * t3 = BN_new();
     /*---计算z1---*/
-    BN_mod_mul(t1,a->CS1.x,b->CS1.x,MOD,CTX);
-    BN_mod_mul(t2,a->CS2.x,b->CS1.x,MOD,CTX);
-    BN_mod_mul(t3,a->CS1.x,b->CS2.x,MOD,CTX);
-    BN_mod_add(t1,t1,t2,MOD,CTX);
-    BN_mod_add(z1,t1,t3,MOD,CTX);
-    /*---计算z2---*/
-    BN_mod_mul(t1,a->CS2.x,b->CS2.x,MOD,CTX);
-    BN_mod_mul(t2,a->CS3.x,b->CS2.x,MOD,CTX);
-    BN_mod_mul(t3,a->CS2.x,b->CS3.x,MOD,CTX);
-    BN_mod_add(t1,t1,t2,MOD,CTX);
-    BN_mod_add(z2,t1,t3,MOD,CTX);
-    /*---计算z3---*/
-    BN_mod_mul(t1,a->CS3.x,b->CS3.x,MOD,CTX);
-    BN_mod_mul(t2,a->CS1.x,b->CS3.x,MOD,CTX);
-    BN_mod_mul(t3,a->CS3.x,b->CS1.x,MOD,CTX);
-    BN_mod_add(t1,t1,t2,MOD,CTX);
-    BN_mod_add(z3,t1,t3,MOD,CTX);
 
+    BN_mul(t1,a->CS1.x,b->CS1.x,CTX);
+
+    BN_mul(t2,a->CS2.x,b->CS1.x,CTX);
+
+    BN_mul(t3,a->CS1.x,b->CS2.x,CTX);
+
+    BN_add(t1,t1,t2);
+    BN_add(z1,t1,t3);
+
+    if(BN_is_negative(z1))
+        neg_flag = 1;
+    BN_set_negative(z1,0);
+    BN_nnmod(z1,z1,MOD,CTX);
+    BN_set_negative(z1,neg_flag);
+    /*---计算z2---*/
+    BN_mul(t1,a->CS2.x,b->CS2.x,CTX);
+
+    BN_mul(t2,a->CS3.x,b->CS2.x,CTX);
+
+    BN_mul(t3,a->CS2.x,b->CS3.x,CTX);
+
+    BN_add(t1,t1,t2);
+    BN_add(z2,t1,t3);
+
+    neg_flag = 0;
+    if(BN_is_negative(z2))
+        neg_flag = 1;
+    BN_set_negative(z2,0);
+    BN_nnmod(z2,z2,MOD,CTX);
+    BN_set_negative(z2,neg_flag);
+    /*---计算z3---*/
+    BN_mul(t1,a->CS3.x,b->CS3.x,CTX);
+
+    BN_mul(t2,a->CS1.x,b->CS3.x,CTX);
+
+    BN_mul(t3,a->CS3.x,b->CS1.x,CTX);
+
+    BN_add(t1,t1,t2);
+    BN_add(z3,t1,t3);
+
+    neg_flag = 0;
+    if(BN_is_negative(z3))
+        neg_flag = 1;
+    BN_set_negative(z3,0);
+    BN_nnmod(z3,z3,MOD,CTX);
+    BN_set_negative(z3,neg_flag);
     // 通过相乘得到的值
     res->is_multi_res = 1;
 
--- eTPSS_test.c
+++ eTPSS_test.c
@@ -11,43 +11,59 @@ int main(int argc, char **argv){
     BN_CTX_start(ctx);
 
     BIGNUM *t1,*t2,*t3;
+
     t1 = BN_CTX_get(ctx);
     t2 = BN_CTX_get(ctx);
     t3 = BN_CTX_get(ctx);
-    eTPSS a,b,c;
     // 初始化mod
     initialize_Constant();
+
+    /*BN_set_word(t1,1111);
+    BN_set_negative(t1,1);
+    BN_set_word(t2,2222);
+    BN_set_negative(t2,1);
+    BN_mul(t3,t1,t2,CTX);
+    char * res = BN_bn2dec(t3);
+    fprintf(stdout,"res:%s",res);
+    fflush(stdout);*/
+
+    eTPSS a,b,c;
     init_eTPSS(&a);
     init_eTPSS(&b);
     init_eTPSS(&c);
     /*-------Share---------*/
     BN_set_word(t1,1111);
+    BN_set_negative(t1,1);
     BN_set_word(t2,2222);
-
+    BN_set_negative(t2,1);
     if(et_Share(&a,t1) != ETPSS_SUCCESS){
         fprintf(stderr,"error in Share operation");
     }
     char *x1 = BN_bn2dec(a.CS1.x);
     char *x2 = BN_bn2dec(a.CS2.x);
     char *x3 = BN_bn2dec(a.CS3.x);
-    fprintf(stdout,"[A]-->total:%d, x1:%s, x2:%s, x3:%s\n",1111,x1,x2,x3);
+    char *str1 = BN_bn2dec(t1);
+    fprintf(stdout,"[A]-->total:%s, x1:%s, x2:%s, x3:%s\n",str1,x1,x2,x3);
     fflush(stdout);  // 刷新标准输出流
     et_Share(&b,t2);
     x1 = BN_bn2dec(b.CS1.x);
     x2 = BN_bn2dec(b.CS2.x);
     x3 = BN_bn2dec(b.CS3.x);
-    fprintf(stdout,"[B]-->total:%d, x1:%s, x2:%s, x3:%s\n",2222,x1,x2,x3);
+    str1 = BN_bn2dec(t2);
+    fprintf(stdout,"[B]-->total:%s, x1:%s, x2:%s, x3:%s\n",str1,x1,x2,x3);
     fflush(stdout);  // 刷新标准输出流
     /*---------Recover---------*/
     if(et_Recover(t3,&a) == ETPSS_ERROR){
         fprintf(stderr,"error in Recover operation");
     }
     x1 = BN_bn2dec(t3);
-    fprintf(stdout,"[A = 1111]-->Recover value is %s\n",x1);
+    str1 = BN_bn2dec(t1);
+    fprintf(stdout,"[A = %s]-->Recover value is %s\n",str1,x1);
     fflush(stdout);  // 刷新标准输出流
     et_Recover(t3,&b);
     x1 = BN_bn2dec(t3);
-    fprintf(stdout,"[B = 2222]-->Recover value is %s\n",x1);
+    str1 = BN_bn2dec(t2);
+    fprintf(stdout,"[B = %s]-->Recover value is %s\n",str1,x1);
     fflush(stdout);  // 刷新标准输出流
     /*---------Add---------*/
     if(et_Add(&c,&a,&b) == ETPSS_ERROR){
@@ -55,7 +71,9 @@ int main(int argc, char **argv){
     }
     et_Recover(t3,&c);
     x1 = BN_bn2dec(t3);
-    fprintf(stdout,"[A = 1111] + [B = 2222] = C-->Recover value is %s\n",x1);
+    str1 = BN_bn2dec(t1);
+    char * str2 = BN_bn2dec(t2);
+    fprintf(stdout,"[A = %s] + [B = %s] = C-->Recover value is %s\n",str1,str2,x1);
     fflush(stdout);  // 刷新标准输出流
     /*---------ScalP---------*/
     BN_set_word(t3,3333);
@@ -65,7 +83,8 @@ int main(int argc, char **argv){
     }
     et_Recover(t3,&c);
     x1 = BN_bn2dec(t3);
-    fprintf(stdout,"3333 * [A = 1111] = C-->Recover value is %s\n",x1);
+
+    fprintf(stdout,"3333 * [A = -1111] = C-->Recover value is %s\n",x1);
     fflush(stdout);  // 刷新标准输出流
     /*---------et_Mul-----------*/
     if(et_Mul(&c,&a,&b) == ETPSS_ERROR){
@@ -73,14 +92,14 @@ int main(int argc, char **argv){
     }
     et_Recover(t3,&c);
     x1 = BN_bn2dec(t3);
-    fprintf(stdout,"[A = 1111] * [B = 2222] = C-->Recover value is %s\n",x1);
+    fprintf(stdout,"[A = -1111] * [B = -2222] = C-->Recover value is %s\n",x1);
     fflush(stdout);  // 刷新标准输出流
     if(et_Mul(&a,&c,&b)== ETPSS_ERROR){
         fprintf(stderr,"error in mul operation");
     }
     et_Recover(t3,&a);
     x1 = BN_bn2dec(t3);
-    fprintf(stdout,"[C = [A] * [B]] * [B = 2222] = C-->Recover value is %s\n",x1);
+    fprintf(stdout,"[C = [A] * [B]] * [B = -2222] = C-->Recover value is %s\n",x1);
     fflush(stdout);  // 刷新标准输出流
     if(et_Mul(&b,&a,&c) == ETPSS_ERROR){
         fprintf(stderr,"error in mul operation");
